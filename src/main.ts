cb0e53ac8f548163f84888462c3bdd1c9bec5673
Gaston Roxas <gacaro51597@gmail.com>
2023-03-10 10:36:38 +0800

    add helmet protection and reorder cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './common/filters/prisma/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://real-estate-app-front.herokuapp.com',
+    ],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -13,13 +25,6 @@ async function bootstrap() {
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
-
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://real-estate-app-front.herokuapp.com',
-    ],
-  });
 
   await app.listen(process.env.PORT || 8080);
 }
