1ccb05bb99d543d302864f8fa0c5ed02d3f5b345
Gaston Roxas <gacaro51597@gmail.com>
2023-03-10 12:38:52 +0800

    refactor cors again

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,17 @@ import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './common/filters/prisma/prisma-client-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:3000',
-        'https://real-estate-app-front.herokuapp.com',
-      ],
-      credentials: true,
-    },
-  });
+  const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://real-estate-app-front.herokuapp.com',
+    ],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
